3cede6ffab73c98a1df8880bf9aee12ccc26ae0a
jomeswang <2019285005@email.szu.edu.cn>
2022-02-12 21:40:21 +0800

    feat: 增加排行榜模块

--- src/game/pages/range.tsx
+++ src/game/pages/range.tsx
@@ -1,3 +1,31 @@
-const Range = () => <div>这是排行榜页面</div>;
+import './range.less';
+
+const Range = () => {
+  const arr = [
+    {
+      name: 'susu',
+      score: 100,
+    },
+    {
+      name: 'susu1',
+      score: 200,
+    },
+    {
+      name: 'susu2',
+      score: 300,
+    },
+  ];
+  const list = () =>
+    arr.map((item, index) => (
+      <div key={index}>
+        {item.name} ----- {item.score}
+      </div>
+    ));
+  return (
+    <div className="flex justify-center items-center">
+      <div className="flex flex-col">{list()}</div>{' '}
+    </div>
+  );
+};
 
 export default Range;
